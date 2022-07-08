6584aa28e7bc6ca9fc7107e1b9a08c2a47b890cf
jiwon kim <jiwon@jiwonui-MacBookAir.local>
2022-07-08 17:15:06 +0900

    todolist array

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
+const todos = [];
+// 뭐든지 텍스트로 만들어 주는 JSON.stringify()
+// stringfy된 값을 js가 인식할 수 있게 하는  parse
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 function handleClickDelete(event) {
   //어디서 클릭이 발생했는지 파악하기 위해서 console.dir(event.target)  확인 -> parnetElement -> innerText
   const li = event.target.parentElement;
@@ -30,8 +39,23 @@ function onTodoSubmit(event) {
   const newTodo = todoInput.value;
   //todo 를 입렵하고 난 뒤에 input 창을 비워주기
   todoInput.value = "";
+  todos.push(newTodo);
+
   //그리고 newTodo를 paintTodo에 넣어서 실행
   paintTodo(newTodo);
+
+  //todo array에 추가된 내용들을 저장
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", onTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+console.log(savedTodos);
+
+if (savedTodos !== null) {
+  //문자열로 변환된 것을 활용할 수 있는 array로 만들어주기 (parse)
+  const parsedTodos = JSON.parse(savedTodos);
+  //parse된 배열의 각각의 요소에 함수 적용 (arrow function사용)
+  parsedTodos.forEach((item) => console.log("say hello" + item));
+}
